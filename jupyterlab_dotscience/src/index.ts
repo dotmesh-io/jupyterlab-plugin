a23ce674741c00dad8a95006f6c6119e28fb3355
Kai Davenport <kaiyadavenport@gmail.com>
2018-05-23 16:54:20 +0100

    show left hand list of commits

--- jupyterlab_dotscience/src/index.ts
+++ jupyterlab_dotscience/src/index.ts
@@ -1,19 +1,69 @@
 import {
-  JupyterLab, JupyterLabPlugin
+  ILayoutRestorer, JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
+
+/*
+import {
+  each
+} from '@phosphor/algorithm';
+*/
+
+import {
+  TabBar, Widget, Title
+} from '@phosphor/widgets';
 
 import '../style/index.css';
 
+const LIST_DATA = [
+  "apples",
+  "oranges",
+]
 
-/**
- * Initialization data for the jupyterlab_dotscience extension.
- */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_dotscience',
+const plugin: JupyterLabPlugin<void> = {
+  id: 'jupyterlab_dotscience_plugin',
+  activate: (app: JupyterLab, restorer: ILayoutRestorer): void => {
+    const { shell } = app;
+    const tabs = new TabBar<Widget>({ orientation: 'vertical' });
+    const header = document.createElement('header');
+
+    restorer.add(tabs, 'dotscience-manager');
+    tabs.id = 'dotscience-manager';
+    tabs.title.label = 'Dotscience';
+    header.textContent = 'Commits';
+    tabs.node.insertBefore(header, tabs.contentNode);
+    shell.addToLeftArea(tabs, { rank: 50 });
+
+    app.restored.then(() => {
+      const populate = () => {
+        tabs.clearTabs();
+
+        LIST_DATA.forEach(title => {
+          const tabTitle = new Title<Widget>({label: title, owner: tabs})
+          tabs.addTab(tabTitle)
+        })
+
+//        each(shell.widgets('main'), widget => { tabs.addTab(widget.title); });
+      };
+
+      // Connect signal handlers.
+      shell.layoutModified.connect(() => { populate(); });
+      tabs.tabActivateRequested.connect((sender, tab) => {
+        //shell.activateById(tab.title.owner.id);
+        console.log('-------------------------------------------');
+        console.log('-------------------------------------------');
+        console.log(`activate tab: ${tab}`)
+        console.dir(tab)
+      });
+      tabs.tabCloseRequested.connect((sender, tab) => {
+        tab.title.owner.close();
+      });
+
+      // Populate the tab manager.
+      populate();
+    });
+  },
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab_dotscience is activated!');
-  }
+  requires: [ILayoutRestorer]
 };
 
-export default extension;
+export default plugin;
