b2e863e64f1f62665cab9e23d8859952df0de136
Charlotte Godley <charlotte@dotmesh.com>
2019-02-18 14:00:17 +0000

    #9: script I nicked from dotscience-python for consistency

---
+++ shipit-pypi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -ex
+
+if [ x$CI_DOCKER_TAG == x ]
+then
+    # Non-CI build
+    CI_DOCKER_TAG=latest
+fi
+
+TWINE_ARGS="$@"
+
+echo "### Building base container"
+
+BASE=dotscience-python-base:$CI_DOCKER_TAG
+
+docker build -t $BASE -f Dockerfile.base .
+
+echo "### Shipping package to PyPi"
+
+docker run $BASE /bin/bash -c "pip install twine ; cd dsbuild ; python3 setup.py sdist bdist_wheel ; twine upload $TWINE_ARGS dist/*"
