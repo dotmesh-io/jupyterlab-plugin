bedf4a10d1af09c1bc41f10264e394f3eb10f5ac
Charlotte Godley <charlotte@dotmesh.com>
2019-10-25 12:22:32 +0100

    dotmesh-io/ops#168: cut versioneer

--- jupyterlab_dotscience_backend/jupyterlab_dotscience_backend/__init__.py
+++ jupyterlab_dotscience_backend/jupyterlab_dotscience_backend/__init__.py
@@ -146,8 +146,3 @@ class CommitterStatusProxy(APIHandler):
         r = requests.get(COMMITTER_STATUS_URL)
         log_stdout(json.dumps(r.json()))
         self.finish(json.dumps(r.json()))
-
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
