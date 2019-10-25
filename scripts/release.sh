c36cfeeb222b4ce4313227b943147380efed119a
Charlotte Godley <charlotte@dotmesh.com>
2019-10-25 12:29:57 +0100

    Bump to version 0.2.20

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 VERSION=$1
-cd jupyterlab_dotscience_backend && bump2version --new-version $VERSION
+cd jupyterlab_dotscience_backend && bump2version --new-version $VERSION patch
 cd ../jupyterlab_dotscience && npm --no-git-tag-version version $VERSION
 git commit -am "Bump to version ${VERSION}" && git tag $VERSION
 git push && git push --tags
