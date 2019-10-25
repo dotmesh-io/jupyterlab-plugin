231510ac07f68fbbd7ccdf0d618d0b9cb32fcef0
Charlotte Godley <charlotte@dotmesh.com>
2019-10-25 12:26:33 +0100

    dotmesh-io/ops#168: switch to committing all in one go

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 VERSION=$1
 cd jupyterlab_dotscience_backend && bump2version --new-version $VERSION
-cd ../jupyterlab_dotscience && npm --no-git-tag-version version from-git
+cd ../jupyterlab_dotscience && npm --no-git-tag-version version $VERSION
+git commit -am "Bump to version ${VERSION}" && git tag $VERSION
 git push && git push --tags
