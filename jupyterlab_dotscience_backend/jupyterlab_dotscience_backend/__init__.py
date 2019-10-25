3f0f0fa6a5c17965d8228b2edb2f31f8659f2189
Charlotte Godley <charlotte@dotmesh.com>
2019-10-25 12:30:40 +0100

    Bump to version 0.2.20

--- jupyterlab_dotscience_backend/jupyterlab_dotscience_backend/__init__.py
+++ jupyterlab_dotscience_backend/jupyterlab_dotscience_backend/__init__.py
@@ -4,7 +4,7 @@ JupyterLab dotscience: proxy through to local dotscience instance
 
 from notebook.utils import url_path_join
 
-__version__ = '0.2.19'
+__version__ = '0.2.20'
 
 import os, json
 from tornado import web
