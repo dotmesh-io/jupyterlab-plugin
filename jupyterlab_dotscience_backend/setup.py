d5e95384445c5decc25b9b4ea98f02a6e755f815
Itamar Turner-Trauring <itamar@itamarst.org>
2019-11-26 14:47:02 -0500

    Bump to version 0.2.22

--- jupyterlab_dotscience_backend/setup.py
+++ jupyterlab_dotscience_backend/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jupyterlab_dotscience_backend",
-    version='0.2.20',
+    version='0.2.22',
     author="dotmesh",
     author_email="luke@dotmesh.com",
     description="Backend component for dotmesh",
